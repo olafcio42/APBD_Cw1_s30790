44f2e1986cd1f70ce46a552d63b2454ac1c6038b
olafcio42 <olafolsf@wp.pl>
2026-03-25 16:55:50 +0100

    initial push

---
+++ Models/Sprzet.cs
@@ -0,0 +1,7 @@
+namespace APBD_Cw1_s30790.Models
+{
+    public class Sprzet
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+namespace APBD_Cw1_s30790
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
