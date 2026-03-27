b123f75ecca61181de5be34df89b14519ad44e95
olafcio42 <olafolsf@wp.pl>
2026-03-27 17:39:08 +0100

    all vars

--- Program.cs
+++ Program.cs
@@ -9,6 +9,28 @@ namespace APBD_Cw1_s30790
     {
         public static void Main(string[] args)
         {
+            var dzisiaj = DateTime.Now;
+            var kalkulator = new StalyKalkulatorKar(10.0m);
+            var wypozyczalnia = new WypozyczalniaService(kalkulator);
+
+            var laptop1 = new Laptop { Nazwa = "Dell XPS", PamiecRamGB = 16, Procesor = "i7" };
+            var kamera1 = new Kamera { Nazwa = "Sony Alpha", FormatNagrywania = "4K", WymiennyObiektyw = true };
+            var projektor1 = new Projektor { Nazwa = "Epson", JasnoscANSI = 3000, Rozdzielczosc = "1080p" };
+            var uszkodzonyLaptop = new Laptop { Nazwa = "Lenovo ThinkPad", PamiecRamGB = 8, Procesor = "i5" };
+            
+            
+            wypozyczalnia.DodajSprzet(laptop1);
+            wypozyczalnia.DodajSprzet(kamera1);
+            wypozyczalnia.DodajSprzet(projektor1);
+            wypozyczalnia.DodajSprzet(uszkodzonyLaptop);
+
+            var student = new Student { Imie = "Jan", Nazwisko = "Kowalski" };
+            var pracownik = new Pracownik { Imie = "Anna", Nazwisko = "Nowak" };
+
+            wypozyczalnia.DodajUzytkownika(student);
+            wypozyczalnia.DodajUzytkownika(pracownik);
+            
+            
             
         }
     }
